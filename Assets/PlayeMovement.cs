18cb0c5ba73f40b7b785cf4b8dab728166f27172
NicoUmansky <umanskynico@gmail.com>
2022-05-17 12:08:13 -0300

    Cubito que se mueve

---
+++ Assets/PlayeMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayeMovement : MonoBehaviour
+{
+    public Vector3 movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.eulerAngles.y < 150) {
+            transform.eulerAngles += movement;
+        }
+        
+    }
+}
